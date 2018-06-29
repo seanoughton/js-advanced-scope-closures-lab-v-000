1e523400f4180753b2899644263de2898a7c5bf6
Sean Oughton <seanoughton@gmail.com>
2018-06-29 20:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function produceDrivingRange(blockRange) {
     }
   };
 };
+
+//- Returns a function that then calculates a tip. For example, produceTipCalculator(.10) returns a function that calculates ten percent tip on a fare. produceTipCalculator(.20) returns a function that calculates twenty percent tip on a fare.
+function produceTipCalculator() {
+
+};
