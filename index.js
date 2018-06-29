c8cfad2501668f3991aec8ae7de65e3701decb1f
Sean Oughton <seanoughton@gmail.com>
2018-06-29 19:50:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function produceDrivingRange(blockRange) {
   //blockRange is the number of blocks where something is in range
-
+  console.log(blockRange);
   return function (beginningBlock,endingBlock) {
     let begNum = parseInt(beginningBlock.slice(0,-2), 10);
     let endNum = parseInt(endingBlock.slice(0,-2), 10);
