60ce58c24f119f7af1fe89bec503df51dac377ec
Sean Oughton <seanoughton@gmail.com>
2018-06-29 19:38:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function produceDrivingRange(blockRange) {
   //blockRange is the number of blocks where something is in range
 
   return function (beginningBlock,endingBlock) {
+    console.log(beginningBlock);
     if ( (beginningBlock - endingBlock) <= blockRange ) {
       return `2 blocks out of range`;
     } else {
