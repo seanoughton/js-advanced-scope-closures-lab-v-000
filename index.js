48c020a7a8456211130d7fd6d1e35749159c0575
Sean Oughton <seanoughton@gmail.com>
2018-06-29 20:08:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,9 @@ function produceTipCalculator(tipPerc) {
     return amount * tipPerc;
   };
 };
+
+//createDriver returns a function that returns a class that produces a Driver class. The class has reference to a driverId that is incremented each time a new driver is created. The rest of the code base does not have access to driverId.
+
+function createDriver () {
+
+};
