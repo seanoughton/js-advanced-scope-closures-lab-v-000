a71df1983cf605e27fe999732935571ef4c59dc1
Sean Oughton <seanoughton@gmail.com>
2018-06-29 19:54:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function produceDrivingRange(blockRange) {
 
 
   return function (beginningBlock,endingBlock) {
+    console.log (blockRange);
     let begNum = parseInt(beginningBlock.slice(0,-2), 10);
     let endNum = parseInt(endingBlock.slice(0,-2), 10);
     let distance = endNum - begNum;
