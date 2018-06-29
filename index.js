64a2f857d02c85e7feead235cfe024a234a984da
Sean Oughton <seanoughton@gmail.com>
2018-06-29 19:26:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Returns a function that then calculates whether a given trip is within range. For example, produceDrivingRange(10) returns a function that will return false if the trip is over 10 blocks distance and true if the distance is within range. So produceDrivingRange returns a function that we can then use to calculate if a trip is too large for a driver. We recommend referencing the test/indexTest.js for more details.
 
-let produceDrivingRange = function () {
+function produceDrivingRange() {
 
 };
